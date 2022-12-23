b845c84c4b528230381d67ba08af465a75f0a5ea
MatheusVict <matheusvictorhenrique@gmail.com>
2022-12-22 21:28:29 -0300

    create, getAll and getOne category, send and emit

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,8 +1,10 @@
 import {
   Body,
   Controller,
+  Get,
   Logger,
   Post,
+  Query,
   UsePipes,
   ValidationPipe,
 } from '@nestjs/common';
@@ -12,8 +14,9 @@ import {
   Transport,
 } from '@nestjs/microservices';
 import { CreateCategoryDTO } from './dtos/create-category.dto';
+import { Observable } from 'rxjs';
 
-@Controller('api/v1')
+@Controller('api/v1/category')
 export class AppController {
   private readonly logger = new Logger(AppController.name);
 
@@ -29,12 +32,19 @@ export class AppController {
     });
   }
 
-  @Post('category')
+  @Post()
   @UsePipes(ValidationPipe)
   async createCategory(@Body() createCategoryDTO: CreateCategoryDTO) {
-    return await this.clientAdminBackend.emit(
-      'criar-categoria',
-      createCategoryDTO,
-    );
+    this.clientAdminBackend.emit('criar-categoria', createCategoryDTO);
+    // Retorna um hot obeservable, msm q a gente não s inscreva o core do nest vai tentar entregar o evento
+  }
+
+  @Get()
+  getOneCategory(@Query('idCategory') idCategory: string): Observable<any> {
+    return this.clientAdminBackend.send(
+      //Retorna um cold observable se inscreve explicitamente antes de enviar
+      'pegar-categoria',
+      idCategory ? idCategory : '',
+    ); //Request/Response
   }
 }
